4772a0fc7aba5a2f8c6da2352e60ce2ae82c5465
surasakcho <surasak.cho@gmail.com>
2023-08-24 09:58:59 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+
 setup(
-    name='icedumpy_geohelper',
+    name='geohelper',
     version='0.0',
     description="Helper package for PIER project",
-    url='#',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/icedumpy/geohelper",
     author='Pongporamat',
     author_email='pongporamat.c@gmail.com',
-    license='None',
-    packages=['icedumpy_geohelper'],
-    zip_safe=False
+    packages=find_packages(),    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
